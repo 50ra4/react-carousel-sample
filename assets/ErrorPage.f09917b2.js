96928fca965729c92612fbe0d58385512c9fd8bd
50ra4 <50ra4@users.noreply.github.com>
2022-10-30 10:37:46 +0000

    deploy: 785821021a3b82913254e843e240f99c8b9e0971

---
+++ assets/ErrorPage.f09917b2.js
@@ -0,0 +1,1 @@
+import{a,j as r,L as e}from"./index.8340f86c.js";function o(){return a("main",{children:[r("h2",{children:"Error Page"}),r(e,{to:"/",children:"TopPage"})]})}export{o as default};
---
+++ assets/TopPage.2b770b3d.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index.8340f86c.js";function a(){return e("main",{children:e("h2",{children:"TopPage"})})}export{a as default};
