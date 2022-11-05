6f0ebc933a914754d00468acee757acccc5ca445
50ra4 <50ra4@users.noreply.github.com>
2022-11-05 08:54:06 +0000

    deploy: e22e43957cdd14c239a94e10dceb1db2776ac6ea

---
+++ storybook/assets/SampleComponent.stories.160d3ca4.js
@@ -0,0 +1,2 @@
+var t=Object.defineProperty;var o=(e,n)=>t(e,"name",{value:n,configurable:!0});import{j as m}from"./jsx-runtime.bcfebb72.js";import"./iframe.09e6959a.js";const p=o(()=>m("div",{children:"Sample Component"}),"SampleComponent"),l={component:p},c={args:{}},d=["Example"];export{c as Example,d as __namedExportsOrder,l as default};
+//# sourceMappingURL=SampleComponent.stories.160d3ca4.js.map
--- storybook/assets/SampleComponent.stories.da7307aa.js
+++
@@ -1,2 +0,0 @@
-var t=Object.defineProperty;var o=(e,n)=>t(e,"name",{value:n,configurable:!0});import{j as m}from"./jsx-runtime.51789e05.js";import"./iframe.7e0574f0.js";const p=o(()=>m("div",{children:"Sample Component"}),"SampleComponent"),l={component:p},c={args:{}},d=["Example"];export{c as Example,d as __namedExportsOrder,l as default};
-//# sourceMappingURL=SampleComponent.stories.da7307aa.js.map
