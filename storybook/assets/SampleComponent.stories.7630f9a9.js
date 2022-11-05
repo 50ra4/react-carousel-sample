ef32db37baa451107a161f32bf8016787f9819e1
50ra4 <50ra4@users.noreply.github.com>
2022-11-05 10:38:46 +0000

    deploy: 2e41a5443624933b7abf03e6d5475157956362ed

---
+++ storybook/assets/SampleComponent.stories.7630f9a9.js
@@ -0,0 +1,2 @@
+var t=Object.defineProperty;var o=(e,n)=>t(e,"name",{value:n,configurable:!0});import{j as m}from"./jsx-runtime.2f76b489.js";import"./iframe.797223e7.js";const p=o(()=>m("div",{children:"Sample Component"}),"SampleComponent"),l={component:p},c={args:{}},d=["Example"];export{c as Example,d as __namedExportsOrder,l as default};
+//# sourceMappingURL=SampleComponent.stories.7630f9a9.js.map
--- storybook/assets/SampleComponent.stories.dcf5be2a.js
+++
@@ -1,2 +0,0 @@
-var t=Object.defineProperty;var o=(e,n)=>t(e,"name",{value:n,configurable:!0});import{j as m}from"./jsx-runtime.7e156888.js";import"./iframe.71fb1e69.js";const p=o(()=>m("div",{children:"Sample Component"}),"SampleComponent"),l={component:p},c={args:{}},d=["Example"];export{c as Example,d as __namedExportsOrder,l as default};
-//# sourceMappingURL=SampleComponent.stories.dcf5be2a.js.map
