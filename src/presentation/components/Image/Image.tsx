5cf619fe18c26513780856276221beb02490be51
50ra4 <50ra4.dev.dev@gmail.com>
2022-11-06 20:10:28 +0900

    add: Image to component

---
+++ src/presentation/components/Image/Image.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export function Image({
+  className,
+  src,
+  caption,
+}: {
+  className?: string;
+  src: string;
+  caption: string;
+}) {
+  return (
+    <ImageWrapper className={className}>
+      <img src={src} alt={caption} />
+    </ImageWrapper>
+  );
+}
+
+const ImageWrapper = styled.div`
+  position: relative;
+  height: 100%;
+  width: 100%;
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    box-shadow: 0 0 3px 1px rgba(0, 0, 0, 0.1) inset;
+  }
+
+  & > img {
+    object-fit: contain;
+    height: 100%;
+    width: 100%;
+  }
+`;
