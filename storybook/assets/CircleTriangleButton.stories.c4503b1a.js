9c9f7029213b5dc8fbf4f356f2392fb924db0505
50ra4 <50ra4@users.noreply.github.com>
2022-11-09 00:37:46 +0000

    deploy: 3b2beefc07c0b261bb6b88f85389212ffdf5c7ef

---
+++ storybook/assets/CircleTriangleButton.stories.c4503b1a.js
@@ -0,0 +1,2 @@
+import{C as o}from"./CircleTriangleButton.b38e68db.js";import"./styled-components.browser.esm.a772b670.js";import"./jsx-runtime.918f70d6.js";import"./iframe.04ea4b46.js";const e={component:o},a={args:{onClick:()=>{},children:"sample main"}},s=["Main"];export{a as Main,s as __namedExportsOrder,e as default};
+//# sourceMappingURL=CircleTriangleButton.stories.c4503b1a.js.map
--- storybook/assets/CircleTriangleButton.stories.e8afc915.js
+++
@@ -1,2 +0,0 @@
-import{C as o}from"./CircleTriangleButton.52a29677.js";import"./styled-components.browser.esm.65a25bd2.js";import"./jsx-runtime.2a26a7f6.js";import"./iframe.dfb9a547.js";const e={component:o},a={args:{onClick:()=>{},children:"sample main"}},s=["Main"];export{a as Main,s as __namedExportsOrder,e as default};
-//# sourceMappingURL=CircleTriangleButton.stories.e8afc915.js.map
