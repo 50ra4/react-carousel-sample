eab2b537e88c95f7c2e40050d8930e88f814c61e
50ra4 <50ra4.dev.dev@gmail.com>
2022-11-09 22:07:24 +0900

    add: TopPage sample

--- src/presentation/pages/TopPage/TopPage.tsx
+++ src/presentation/pages/TopPage/TopPage.tsx
@@ -1,11 +1,78 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Carousel } from '@/components/Carousel/Carousel';
+import { CarouselSampleSlide } from '@/components/Carousel/CarouselSampleSlide';
+import { SubSection } from '@/components/SubSection/SubSection';
+
+const Array4 = Array.from({ length: 4 }).map((_, i) => i + 1);
 
 function TopPage() {
   return (
-    <main>
-      <h2>TopPage</h2>
-    </main>
+    <Root>
+      <ResponsiveContainer>
+        <Title>Carousel Sample</Title>
+        <StyledSubSection title="basic">
+          <Carousel carouselKey="basic">
+            {Array4.map((count) => (
+              <CarouselSampleSlide key={count} count={count} />
+            ))}
+          </Carousel>
+        </StyledSubSection>
+
+        <StyledSubSection title="autoplay 5s">
+          <Carousel carouselKey="autoplay-5s" autoplay={5000}>
+            {Array4.map((count) => (
+              <CarouselSampleSlide key={count} count={count} />
+            ))}
+          </Carousel>
+        </StyledSubSection>
+
+        <StyledSubSection title="autoplay 3s">
+          <Carousel carouselKey="autoplay-3s" autoplay={3000}>
+            {Array4.map((count) => (
+              <CarouselSampleSlide key={count} count={count} />
+            ))}
+          </Carousel>
+        </StyledSubSection>
+
+        <StyledSubSection title="multiple children">
+          <Carousel carouselKey="multiple">
+            {Array4.map((count) => (
+              <CarouselSampleSlide key={count} count={count} />
+            ))}
+            {Array4.map((count) => (
+              <CarouselSampleSlide key={count + 4} count={count + 4} />
+            ))}
+          </Carousel>
+        </StyledSubSection>
+      </ResponsiveContainer>
+    </Root>
   );
 }
+
+const Root = styled.main`
+  background-color: #eee;
+`;
+
+const Title = styled.h2`
+  background-color: #fff;
+  text-align: center;
+  line-height: 48px;
+  font-weight: bold;
+  font-size: 32px;
+  padding: 8px 0;
+`;
+
+const StyledSubSection = styled(SubSection)`
+  background-color: #fff;
+  margin-top: 8px;
+`;
+
+const ResponsiveContainer = styled.div`
+  max-width: 400px;
+  @media (min-width: 400px) {
+    margin: 0 auto;
+  }
+`;
 
 export default TopPage;
