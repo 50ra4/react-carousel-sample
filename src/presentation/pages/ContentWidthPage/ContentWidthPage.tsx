c069a24308ef35bf6a202a69fae728422191ba28
50ra4 <50ra4.dev.dev@gmail.com>
2022-12-10 18:31:33 +0900

    add: ContentWidthPage

---
+++ src/presentation/pages/ContentWidthPage/ContentWidthPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { PageTemplate } from 'src/presentation/PageTemplate';
+
+function ContentWidthPage() {
+  return <PageTemplate title="Check useContentWidth">null</PageTemplate>;
+}
+
+export default ContentWidthPage;
--- src/presentation/routes.tsx
+++ src/presentation/routes.tsx
@@ -2,6 +2,10 @@ import React, { lazy } from 'react';
 import { RouteProps } from 'react-router-dom';
 
 const TopPage = lazy(() => import('src/presentation/pages/TopPage/TopPage'));
+const ContentWidthPage = lazy(
+  () => import('src/presentation/pages/ContentWidthPage/ContentWidthPage'),
+);
+
 export const ErrorPage = lazy(
   () => import('src/presentation/pages/ErrorPage/ErrorPage'),
 );
@@ -10,6 +14,10 @@ export const ROUTES: RouteProps[] = [
   {
     path: '/',
     element: <TopPage />,
+  },
+  {
+    path: '/content-width',
+    element: <ContentWidthPage />,
   },
   {
     path: '/error',
