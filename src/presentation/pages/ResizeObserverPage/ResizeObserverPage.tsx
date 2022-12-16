5777dd5c99af5dc9275e1d6f6a99dc96cb1ffff4
50ra4 <50ra4.dev.dev@gmail.com>
2022-12-17 01:36:53 +0900

    update: ResizeObserverPage

--- src/presentation/pages/ResizeObserverPage/ResizeObserverPage.tsx
+++ src/presentation/pages/ResizeObserverPage/ResizeObserverPage.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useMemo, useRef } from 'react';
 import styled from 'styled-components';
 import { useResizeObserver } from 'src/hooks/useResizeObserver';
 import { PageTemplate } from 'src/presentation/PageTemplate';
@@ -6,24 +6,144 @@ import { PageTemplate } from 'src/presentation/PageTemplate';
 function ResizeObserverPage() {
   return (
     <PageTemplate title="ResizeObserver">
-      <ObservedBox />
+      <ObservedBox color="aliceblue" />
+      <ObservedBox color="lemonchiffon" withMargin={true} />
+      <ObservedBox color="mistyrose" contentBox={true} />
+      <ObservedBox color="lavender" withMargin={true} contentBox={true} />
     </PageTemplate>
   );
 }
 
-const ObservedBox = ({ className }: { className?: string }) => {
-  const ref = useRef(null);
-  useResizeObserver(ref, { duration: 100 });
-
-  return (
-    <Container ref={ref} className={className}>
-      ObservedBox
-    </Container>
-  );
+type Props = {
+  color: 'aliceblue' | 'lemonchiffon' | 'mistyrose' | 'lavender';
+  contentBox?: boolean;
+  withMargin?: boolean;
 };
 
-const Container = styled.div`
-  display: flex;
+const toNameSize = <T extends Record<string, number>>(obj: T) =>
+  Object.entries(obj).map(([name, size]) => ({ name, size }));
+
+function ObservedBox({
+  className,
+  ...props
+}: {
+  className?: string;
+} & Props) {
+  const ref = useRef(null);
+  const entry = useResizeObserver(ref, { duration: 100 });
+
+  const definitionList = useMemo((): Definition[] => {
+    if (!entry) {
+      return [];
+    }
+
+    const borderBoxSize = entry.borderBoxSize
+      ? { title: 'borderBoxSize', list: toNameSize(entry.borderBoxSize) }
+      : null;
+    const contentBoxSize = entry.contentBoxSize
+      ? {
+          title: 'contentBoxSize',
+          list: toNameSize(entry.contentBoxSize),
+        }
+      : null;
+    const contentRect = entry.contentRect
+      ? {
+          title: 'contentBoxSize',
+          list: toNameSize(entry.contentRect.toJSON()),
+        }
+      : null;
+    const devicePixelContentBoxSize = entry.devicePixelContentBoxSize
+      ? {
+          title: 'devicePixelContentBoxSize',
+          list: toNameSize(entry.devicePixelContentBoxSize),
+        }
+      : null;
+
+    return [
+      borderBoxSize,
+      contentBoxSize,
+      contentRect,
+      devicePixelContentBoxSize,
+    ].filter((v): v is NonNullable<typeof v> => !!v);
+  }, [entry]);
+
+  return (
+    <Container {...props} ref={ref} className={className}>
+      <ul>
+        <li>box-sizing: {props.contentBox ? 'content-box' : 'border-box'}</li>
+        <li>margin: {props.withMargin ? 'true' : 'false'}</li>
+      </ul>
+      {definitionList.map(({ title, list }) => (
+        <DefinitionSection key={title} title={title} list={list} />
+      ))}
+    </Container>
+  );
+}
+
+type Definition = { title: string; list: { name: string; size: number }[] };
+
+function DefinitionSection({ title, list }: Definition) {
+  if (!list.length) {
+    return null;
+  }
+
+  return (
+    <StyledSection>
+      <h3>{title}</h3>
+      <dl>
+        {list.map(({ name, size }) => (
+          <div key={name}>
+            <dt>{name}</dt>
+            <dd>{size}</dd>
+          </div>
+        ))}
+      </dl>
+    </StyledSection>
+  );
+}
+
+const StyledSection = styled.section`
+  & > h3 {
+    font-size: 18px;
+    font-weight: bold;
+    margin-bottom: 4px;
+  }
+
+  margin: 8px 0;
+  & > dl {
+    border: 1px solid #000;
+    & > div {
+      display: flex;
+      & > * {
+        padding: 2px 4px;
+      }
+
+      & > dt {
+        min-width: 120px;
+        margin-right: 10px;
+      }
+      & > dd {
+        border-left: 1px solid #000;
+        text-align: right;
+      }
+    }
+    & > div + div {
+      border-top: 1px solid #000;
+    }
+  }
+`;
+
+const Container = styled.div<Props>`
+  width: 100%;
+  padding: 10px 20px;
+  box-sizing: ${({ contentBox }) =>
+    contentBox ? 'content-box' : 'border-box'};
+  & > * {
+    box-sizing: border-box;
+  }
+  border: 5px solid #000;
+  background-color: ${({ color }) => color};
+  margin: ${({ withMargin }) => (withMargin ? '10px 5px' : 0)};
 `;
 
 export default ResizeObserverPage;
