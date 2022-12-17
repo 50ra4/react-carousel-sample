c004d32030de32375dd78855ba1eae1f76f8b3a5
50ra4 <50ra4.dev.dev@gmail.com>
2022-12-17 15:43:24 +0900

    change: use useResizeObserver in useContentWidth

--- src/hooks/useContentWidth.ts
+++ src/hooks/useContentWidth.ts
@@ -1,40 +1,19 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
-import { audit } from 'src/utils/function';
+import React, { useMemo } from 'react';
+import { useResizeObserver } from './useResizeObserver';
+
+type Options = {
+  duration?: number;
+};
 
 export function useContentWidth<T extends HTMLElement = HTMLElement>(
   ref: React.RefObject<T | null>,
+  { duration = 250 }: Options = {},
 ) {
-  const [width, setWidth] = useState<number | null>(null);
-  const perviousWidth = useRef(0);
+  const result = useResizeObserver(ref, { duration });
 
-  const updatedWidth = useCallback((width: number) => {
-    if (width === perviousWidth.current) {
-      return;
-    }
-    setWidth(width);
-    perviousWidth.current = width;
-  }, []);
-
-  useEffect(() => {
-    if (!ref.current) {
-      return;
-    }
-
-    const update = audit(updatedWidth, 250);
-    const callback: ResizeObserverCallback = (entries) => {
-      entries.forEach((entry) => {
-        update(entry.contentRect.width);
-      });
-    };
-
-    const element = ref.current;
-    const observer = new ResizeObserver(callback);
-    observer.observe(element);
-
-    return () => {
-      observer.unobserve(element);
-    };
-  }, [ref, updatedWidth]);
-
-  return width;
+  return useMemo(
+    () =>
+      result?.borderBoxSize?.inlineSize ?? result?.contentRect?.width ?? null,
+    [result],
+  );
 }
