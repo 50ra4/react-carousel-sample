92cc7e45c6c4aa17879f97c27f07f6445665416e
50ra4 <50ra4@users.noreply.github.com>
2022-12-17 08:43:09 +0000

    deploy: 2caf5b5efbc11ff8994059d04552275a24cd87b1

---
+++ storybook/assets/CarouselSampleSlide.2f926d2c.js
@@ -0,0 +1,14 @@
+var n=Object.defineProperty;var o=(e,a)=>n(e,"name",{value:a,configurable:!0});import{H as s}from"./styled-components.browser.esm.80a5d116.js";import{j as r}from"./jsx-runtime.4b1fe029.js";function l({className:e,count:a}){return r(t,{className:e,even:a%2===0,children:r("span",{children:a})})}o(l,"CarouselSampleSlide");const t=s.div`
+  flex: 0 0 100%;
+  height: 100%;
+  background-color: ${({even:e})=>e?"#99f":"#f99"};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & > * {
+    color: #fff;
+    font-size: 2em;
+  }
+`;try{l.displayName="CarouselSampleSlide",l.__docgenInfo={description:"",displayName:"CarouselSampleSlide",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},count:{defaultValue:null,description:"",name:"count",required:!0,type:{name:"number"}}}},typeof STORYBOOK_REACT_CLASSES<"u"&&(STORYBOOK_REACT_CLASSES["src/presentation/components/Carousel/CarouselSampleSlide.tsx#CarouselSampleSlide"]={docgenInfo:l.__docgenInfo,name:"CarouselSampleSlide",path:"src/presentation/components/Carousel/CarouselSampleSlide.tsx#CarouselSampleSlide"})}catch{}export{l as C};
+//# sourceMappingURL=CarouselSampleSlide.2f926d2c.js.map
